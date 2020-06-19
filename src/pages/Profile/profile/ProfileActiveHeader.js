bd5cd0a093f177f60909aa3b5dc00f98479afefe
SokolovaD <51051738+SokolovaD@users.noreply.github.com>
2020-06-20 01:05:53 +0300

    ещё один коммит

--- src/pages/Profile/profile/ProfileActiveHeader.js
+++ src/pages/Profile/profile/ProfileActiveHeader.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function ProfileActiveHeader() {
     let html = <div>
-        <h3>Моя активность</h3>
+        <h3 className="head_osn_prof">Моя активность</h3>
+        <nav>
+            <a>Мои идеи</a>
+            <a>Последняя активность</a>
+            <a></a>
+        </nav>
     </div>
     return html;
 }
